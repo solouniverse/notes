b7e4c5db2dd683a8e95bd19663d52f9dec213a92
Srikanth Myakam <m.srikanth.m@gmail.com>
2017-09-08 21:31:28 -0700

    C Lang 8th Sept

---
+++ C Language/Sept 8th/1.c
@@ -0,0 +1,14 @@
+//Sri Matrenamaha!
+#include <stdio.h>
+int main(){ // void main()
+	int variable;
+	float f_variable;
+	char name[15] = "Sri Rama";
+	variable=1024;
+	f_variable=512.128;
+	printf("Jai Kali\n");
+	printf("Hello World!\n");
+	printf("Variable value: %d\n",variable);
+	printf("f_variable value: %f\n",f_variable);
+	printf("name value: %s\n",name);
+}
