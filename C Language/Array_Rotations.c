bfc36cc0198fea5a7edabd83dc88abd4361aa04a
Srikanth Myakam <m.srikanth.m@gmail.com>
2017-09-17 23:46:57 -0700

    Ouput added

--- C Language/Array_Rotations.c
+++ C Language/Array_Rotations.c
@@ -119,3 +119,42 @@ int main()
 	printArrayRightToLeftUpsideDown_or_Rotate_180(iArray);
 	printArrayprintArrayVerticalUpsideDown_Rotate_270(iArray);
 }
+/*
+[v-srm.MININT-7RKRDIO] ➤ ./a.exe
+
+printArray_or_Horizontal:
+1       2       3
+4       5       6
+7       8       9
+
+printArrayTopDown_or_Vertical:
+1       4       7
+2       5       8
+3       6       9
+
+printArrayUpsideDown_or_HorizontalFlip:
+7       8       9
+4       5       6
+1       2       3
+
+printArrayRightToLeft_or_VerticalFlip:
+3       2       1
+6       5       4
+9       8       7
+
+printArray_Rotate_90:
+7       4       1
+8       5       2
+9       6       3
+
+printArrayRightToLeftUpsideDown_or_Rotate_180:
+9       8       7
+6       5       4
+3       2       1
+
+printArrayprintArrayVerticalUpsideDown_Rotate_270:
+3       6       9
+2       5       8
+1       4       7
+
+*/
