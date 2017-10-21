f0b35c8ca9013adb1812116a4b9bd335c5c4f9b4
Srikanth Myakam <m.srikanth.m@gmail.com>
2017-10-20 23:02:55 -0700

    Fixed Issue

--- Python/Algorithms/Rotate_Array_inPlace.py
+++ Python/Algorithms/Rotate_Array_inPlace.py
@@ -1,4 +1,4 @@
-MAX=6
+MAX=9
 #Array=[[x for x in range (y)] for y in [z*3 for z in range (MAX)]]
 Array=[[x for x in range (y, y+MAX)] for y in range (0,MAX*MAX,MAX)]
 #print(Array)
@@ -15,20 +15,22 @@ for j in range(MAX//2):
 	for i in range(j,MAX-j-1):
 		temp=Array[j][i]
 		Array[j][i]=Array[MAX-1-i][j]
-		Array[MAX-1-i][j]=Array[MAX-j-1][MAX-j-1-i]
-		Array[MAX-j-1][MAX-j-1-i]=Array[i][MAX-j-1]
+		Array[MAX-1-i][j]=Array[MAX-j-1][MAX-1-i]
+		Array[MAX-j-1][MAX-1-i]=Array[i][MAX-j-1]
 		Array[i][MAX-j-1]=temp
-		print("%d-%d:--------------------------------------------"%(j,i))
-		PrintArray(Array)
-	
+#		print("%d-%d:--------------------------------------------"%(j,i))
+#		PrintArray(Array)
+
+PrintArray(Array)
+
 exit()
 
-for j in range(MAX-1):
+for j in range(MAX//2):
 	#for i in range(j,(MAX-j)//2):
 	for i in range(j,MAX-j-1):
 		Array[j][i],Array[i][MAX-1-j]=Array[i][MAX-1-j],Array[j][i]
-		Array[j][i],Array[MAX-1-j][MAX-1-i-j]=Array[MAX-1-j][MAX-1-i-j],Array[j][i]
-		Array[j][i],Array[MAX-1-i-j][j]=Array[MAX-1-i-j][j],Array[j][i]
+		Array[j][i],Array[MAX-1-j][MAX-1-i]=Array[MAX-1-j][MAX-1-i],Array[j][i]
+		Array[j][i],Array[MAX-1-j][j]=Array[MAX-1-j][j],Array[j][i]
 
 PrintArray(Array)
 exit()
