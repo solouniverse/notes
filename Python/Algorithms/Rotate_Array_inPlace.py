47d158895d7e3dad54a9109a984eb18fa9f21004
Srikanth Myakam <m.srikanth.m@gmail.com>
2017-10-21 20:36:34 -0700

    Fixed 2nd method

--- Python/Algorithms/Rotate_Array_inPlace.py
+++ Python/Algorithms/Rotate_Array_inPlace.py
@@ -1,4 +1,4 @@
-MAX=9
+MAX=7
 #Array=[[x for x in range (y)] for y in [z*3 for z in range (MAX)]]
 Array=[[x for x in range (y, y+MAX)] for y in range (0,MAX*MAX,MAX)]
 #print(Array)
@@ -6,25 +6,36 @@ Array=[[x for x in range (y, y+MAX)] for y in range (0,MAX*MAX,MAX)]
 def PrintArray(Array):
 	for x in range(MAX):
 		for y in range(MAX):
-			print("%3d" %Array[x][y], end=" ")
+			#print("%3d (%d,%d)" %(Array[x][y],x,y), end=" ")
+			print("%3d" %(Array[x][y]), end=" ")
 		print()
 	print()
 PrintArray(Array)
 
-for j in range(MAX//2):
-	for i in range(j,MAX-j-1):
-		temp=Array[j][i]
-		Array[j][i]=Array[MAX-1-i][j]
-		Array[MAX-1-i][j]=Array[MAX-j-1][MAX-1-i]
-		Array[MAX-j-1][MAX-1-i]=Array[i][MAX-j-1]
-		Array[i][MAX-j-1]=temp
-#		print("%d-%d:--------------------------------------------"%(j,i))
-#		PrintArray(Array)
+def rotate_90(Array):
+	for j in range(MAX//2):
+		for i in range(j,MAX-j-1):
+			temp=Array[j][i]
+			Array[j][i]=Array[MAX-1-i][j]
+			Array[MAX-1-i][j]=Array[MAX-j-1][MAX-1-i]
+			Array[MAX-j-1][MAX-1-i]=Array[i][MAX-j-1]
+			Array[i][MAX-j-1]=temp
+	#		print("%d-%d:--------------------------------------------"%(j,i))
+	#		PrintArray(Array)
+def rotate_90_2(Array):
+	for j in range(MAX//2):
+		for i in range(j,MAX-j-1):
+			Array[j][i],Array[i][MAX-1-j]=Array[i][MAX-1-j],Array[j][i]
+			Array[j][i],Array[MAX-1-j][MAX-1-i]=Array[MAX-1-j][MAX-1-i],Array[j][i]
+			Array[j][i],Array[MAX-1-i][j]=Array[MAX-1-i][j],Array[j][i]
+			#print("%d-%d:--------------------------------------------"%(j,i))
+			#PrintArray(Array)
 
+rotate_90(Array)
 PrintArray(Array)
-
-exit()
-
+rotate_90_2(Array)
+#exit()
+'''
 for j in range(MAX//2):
 	#for i in range(j,(MAX-j)//2):
 	for i in range(j,MAX-j-1):
@@ -32,8 +43,7 @@ for j in range(MAX//2):
 		Array[j][i],Array[MAX-1-j][MAX-1-i]=Array[MAX-1-j][MAX-1-i],Array[j][i]
 		Array[j][i],Array[MAX-1-j][j]=Array[MAX-1-j][j],Array[j][i]
 
-PrintArray(Array)
-exit()
+
 Array[0][0],Array[0][MAX-1]=Array[0][MAX-1],Array[0][0]
 Array[0][0],Array[MAX-1][MAX-1]=Array[MAX-1][MAX-1],Array[0][0]
 Array[0][0],Array[MAX-1][0]=Array[MAX-1][0],Array[0][0]
@@ -41,5 +51,8 @@ Array[0][0],Array[MAX-1][0]=Array[MAX-1][0],Array[0][0]
 Array[0][1],Array[1][MAX-1]=Array[1][MAX-1],Array[0][1]
 Array[0][1],Array[MAX-1][MAX-1-1]=Array[MAX-1][MAX-1-1],Array[0][1]
 Array[0][1],Array[MAX-1-1][0]=Array[MAX-1-1][0],Array[0][1]
+'''
 
 
+PrintArray(Array)
+exit()
